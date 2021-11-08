8a648d69b07eed0d20f43ce47c3d3bf95ef1b724
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-11-08 21:13:07 +0700

    layouting adjustment

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,33 +1,46 @@
-import React from 'react';
-import { Icon } from '@iconify/react';
-import './Navibar.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Icon } from "@iconify/react";
+import "./Navibar.css";
+import { Link } from "react-router-dom";
 
-class Navibar extends React.Component{
-    render(){
-        return (
-            <div className="sidebarleft">
-                <div className= "navbar">
-                    <div className="companylogo">
-                        <Icon icon="akar-icons:send" className="arrow" />
-                        <Icon icon="ci:trash-empty"  className="trash"/>
-                        <a>Trash <span style={{color :"#50CB93"}}>Submitter</span></a>
-                    </div>
-                    <div className = "user">
-                        <Icon icon="healthicons:ui-user-profile-outline" className="userprofile" />
-                        <a className="username">Alfatih Rizqi Alfian</a>
-                    </div>
-                    <div className = "navmenu">
-                        <Link to ="/" className="dashboard"><Icon icon="ant-design:home-filled" className="home" /><a>Dashboard</a></Link>
-                        <Link to ="/MainSubmit" className="submityourtrash"> <Icon icon="bi:download" className="downloadicon" /><a>Submit your Trash</a></Link>
-                        <Link to = "/MainLeaderboard"className="leaderboard"><Icon icon="akar-icons:trophy" className="trophyicon" /><a>Leaderboards</a></Link>
-                        <Link to ="/History" className="historymenu"><Icon icon="akar-icons:clock" className="historysymbol"/><a>History</a></Link>
-                        <div className="activenavpanel start"></div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+const Navibar = () => {
+  return (
+    <div className="sidebarleft">
+      <div className="companylogo">
+        <Icon icon="akar-icons:send" className="arrow" />
+        <Icon icon="ci:trash-empty" className="trash" />
+        <a>
+          Trash <span style={{ color: "#50CB93" }}>Submitter</span>
+        </a>
+      </div>
+      <div className="user">
+        <Icon
+          icon="healthicons:ui-user-profile-outline"
+          className="userprofile"
+        />
+        <a className="username">Alfatih Rizqi Alfian</a>
+      </div>
+      <div className="navmenu">
+        <Link to="/" className="dashboard">
+          <Icon icon="ant-design:home-filled" className="home" />
+          <a>Dashboard</a>
+        </Link>
+        <Link to="/main-submit" className="submityourtrash">
+          <Icon icon="bi:download" className="downloadicon" />
+          <a>Submit your Trash</a>
+        </Link>
+        <Link to="/main-leaderboard" className="leaderboard">
+          <Icon icon="akar-icons:trophy" className="trophyicon" />
+          <a>Leaderboards</a>
+        </Link>
+        <Link to="/history" className="historymenu">
+          <Icon icon="akar-icons:clock" className="historysymbol" />
+          <a>History</a>
+        </Link>
+        <div className="activenavpanel start"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Navibar ;
+export default Navibar;
